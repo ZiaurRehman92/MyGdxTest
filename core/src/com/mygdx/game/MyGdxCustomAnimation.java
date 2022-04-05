0bf36dd727b1c114b28618085d7078b2087cba7d
Zia <zia@jogo.ai>
2022-04-05 12:16:58 +0500

    animation working with sample sprites

--- core/src/com/mygdx/game/MyGdxCustomAnimation.java
+++ core/src/com/mygdx/game/MyGdxCustomAnimation.java
@@ -4,28 +4,38 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 
 public class MyGdxCustomAnimation extends ApplicationAdapter {
     SpriteBatch batch;
     Texture img;
     AnimationClass animationClass;
+    TextureAtlas textureAtlas;
+    Animation animation;
+    float timePassed=0;
     @Override
     public void create () {
 
         batch = new SpriteBatch();
-        animationClass = new AnimationClass(new Texture(Gdx.files.internal("walkingman.png")));
+        textureAtlas =  new TextureAtlas(Gdx.files.internal("SpriteSheet/walkingboy.atlas"));
+        animation=new Animation(1/10f,textureAtlas.getRegions());
+        //animationClass = new AnimationClass(new Texture(Gdx.files.internal("walkingman.png")));
 
     }
 
     @Override
     public void render () {
 
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        animationClass.draw(batch);
+        timePassed+=Gdx.graphics.getDeltaTime();
+        batch.draw((TextureRegion) animation.getKeyFrame(timePassed,true),500,500);
+      //  animationClass.draw(batch);
         batch.end();
     }
 
